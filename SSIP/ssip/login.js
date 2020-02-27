ab946a36e5fa986d9898241ab5b269f8929326c0
Bhumit Ghadia <bhumit8400@gmail.com>
2020-02-27 11:21:24 +0530

    no message

--- SSIP/ssip/login.js
+++ SSIP/ssip/login.js
@@ -1,20 +1,20 @@
-var attempt = 3; 
-function validate(){
-var resID = document.getElementById("resID").value;
-var password = document.getElementById("password").value;
-if ( resID == "abc" && password == "123"){
-alert ("Login successfully");
-window.location = "abc.html";
-return false;
-}
-else{
-attempt --;
-alert("You have left "+attempt+" attempt;");
-if( attempt == 0){
-document.getElementById("resID").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
+var attempt = 3;
+
+function validate() {
+    var resID = document.getElementById("resID").value;
+    var password = document.getElementById("password").value;
+    if (resID == "abc" && password == "123") {
+        alert("Login successfully");
+        window.location = "SSIP/.html";
+        return false;
+    } else {
+        attempt--;
+        alert("You have left " + attempt + " attempt;");
+        if (attempt == 0) {
+            document.getElementById("resID").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
 }
